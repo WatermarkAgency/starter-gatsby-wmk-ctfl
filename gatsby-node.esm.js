faef15296281fa4ca482bd081663151366872274
peterwatermark <peter@watermarkadvertising.net>
2020-02-17 10:39:48 -0700

    added redirects

--- gatsby-node.esm.js
+++ gatsby-node.esm.js
@@ -1,25 +1,42 @@
-import Theme from './src/vars/ThemeOptions'
+import Theme from "./src/vars/ThemeOptions";
 
-const path = require(`path`)
+const path = require(`path`);
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage, createRedirect } = actions
-  createRedirect({fromPath: '/', toPath: `${Theme.paths.home}`, isPermanent: true, redirectInBrowser: true})
-  const pageTemplate = path.resolve("./src/node/page.js")
+  const { createPage, createRedirect } = actions;
+  createRedirect({
+    fromPath: "/",
+    toPath: `${Theme.paths.home}`,
+    isPermanent: true,
+    redirectInBrowser: true
+  });
+  const pageTemplate = path.resolve("./src/node/page.js");
+  createRedirect({
+    fromPath: "/home",
+    toPath: `/`,
+    isPermanent: true,
+    redirectInBrowser: true
+  });
+  createRedirect({
+    fromPath: `${Theme.paths.home}`,
+    toPath: `/`,
+    isPermanent: true,
+    redirectInBrowser: true
+  });
   return graphql(`
-  {
-    allContentfulPagesDynamic {
-      edges {
-        node {
-          slug
-          title
+    {
+      allContentfulPagesDynamic {
+        edges {
+          node {
+            slug
+            title
+          }
         }
       }
     }
-  }
   `).then(result => {
     if (result.errors) {
-      throw result.errors
+      throw result.errors;
     }
 
     // Create site pages
@@ -28,8 +45,8 @@ exports.createPages = ({ graphql, actions }) => {
         // Path for this page — required
         path: `${edge.node.slug}`,
         component: pageTemplate,
-        context: edge.node,
-      })
-    })
-  })
-}
+        context: edge.node
+      });
+    });
+  });
+};
